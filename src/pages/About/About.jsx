de23e1f9d21e86f739ad90b5c084104a4ba3f433
BuildTools <unconfigured@null.spigotmc.org>
2023-08-02 02:41:28 +0800

    about page test

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import Navbar from '../../components/header/Navbar'
 import Footer from '../../components/footer/Footer'
 import './About.css'
+import fbIcon from '../../assets/fb_icon.png'
 
 function About() {
   return (
-    <>
+    <section  className='a-section'>
       <Navbar/>
-        <div className='a-wrapper'>
-          <h1>About</h1>
-        </div>
+        <div className='a-layout'>
+          <h2>About Us</h2>
+          <img className="a-fb" alt="fb-icon" src={fbIcon} />
+          <p>fbpage@gmail.com</p>
+        </div>  
       <Footer/>
-    </>
+    </section>
   )
 }
 
