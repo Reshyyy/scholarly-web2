f2458ed39afa4b5979559cb1fa1b5b3233d3cdf2
BuildTools <unconfigured@null.spigotmc.org>
2023-08-25 01:18:31 +0800

    ui and dynamic fetching of data in applicant side

--- src/pages/Applicant/ApplicantScholarships.jsx
+++ src/pages/Applicant/ApplicantScholarships.jsx
@@ -1,16 +1,61 @@
-import React from 'react'
-import ApplicantNavbar from '../../components/header/ApplicantNavbar'
-import { Container } from '@mui/material'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Paper, Typography, Button, Container, Box } from '@mui/material';
+import axios from 'axios';
+import ApplicantNavbar from './../../components/header/ApplicantNavbar'
+
 
 const ApplicantScholarships = () => {
+  const [scholarships, setScholarships] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8081/api/data');
+      setScholarships(response.data);
+    } catch (error) {
+      console.error('Error fetching scholarship data:', error);
+    }
+  };
+
   return (
     <div>
-        <ApplicantNavbar />
-        <Container>
-            <h1>Applicant Scholarships</h1>
-        </Container>
+      <ApplicantNavbar />
+      <Container>
+        <Box mt={4}>
+          {scholarships.map((scholarship) => (
+            <Paper key={scholarship.id} elevation={3} style={{ padding: '20px', marginBottom: '20px' }}>
+              <Typography variant="h5" gutterBottom>
+                {scholarship.scholarship_name}
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                Category: {scholarship.category}
+              </Typography>
+              <Typography variant="subtitle1">
+                Description: {scholarship.description}
+              </Typography>
+              <Typography variant="subtitle1">
+                Amount: {scholarship.amount}
+              </Typography>
+              <Box display="flex" justifyContent="center" marginTop="10px">
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  component={Link}
+                  to={`/scholarship/${scholarship.id}`} // Adjust the route as needed
+                >
+                  View Details
+                </Button>
+              </Box>
+            </Paper>
+          ))}
+        </Box>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ApplicantScholarships
+export default ApplicantScholarships;
